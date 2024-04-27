44248dbb66a294a37a4b2160ede164ac835ead56
Jan Volmut <jan.volmut@gmail.com>
2024-04-27 13:43:45 +0800

    Added portfolios controller

---
+++ BACKEND/src/main/java/com/hcltech/hackaton/squadten/portfoliomanagement/api/portfolio/PortfolioController.java
@@ -0,0 +1,17 @@
+package com.hcltech.hackaton.squadten.portfoliomanagement.api.portfolio;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/portfolios")
+public class PortfolioController {
+
+    @PostMapping("/{portfolioId}/addTrade")
+    @ResponseBody
+    public AddTradeResponse addTrade(@PathVariable String portfolioId) {
+        return AddTradeResponse.builder()
+                .portfolioId(portfolioId)
+                .tradeId("123")
+                .build();
+    }
+}
