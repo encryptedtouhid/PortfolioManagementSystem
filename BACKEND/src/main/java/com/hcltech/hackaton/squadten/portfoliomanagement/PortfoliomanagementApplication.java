1ac2f7fd60e29d76bb8f53a2a901ea0779f0443a
Jan Volmut <jan.volmut@gmail.com>
2024-04-27 13:07:37 +0800

    Initial backend

---
+++ BACKEND/src/main/java/com/hcltech/hackaton/squadten/portfoliomanagement/PortfoliomanagementApplication.java
@@ -0,0 +1,13 @@
+package com.hcltech.hackaton.squadten.portfoliomanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfoliomanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfoliomanagementApplication.class, args);
+	}
+
+}
