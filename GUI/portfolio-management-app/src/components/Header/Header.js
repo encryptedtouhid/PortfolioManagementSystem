de87a1faf76548d64204a5c9fffc0e70260677c0
Khaled Md Tuhidul Hossain <me@tuhidulhossain.com>
2024-04-27 16:47:18 +0800

    Customer Info Added

--- GUI/portfolio-management-app/src/components/Header/Header.js
+++ GUI/portfolio-management-app/src/components/Header/Header.js
@@ -1,17 +1,35 @@
-// components/Header.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Header() {
+  const [customerInfo, setCustomerInfo] = useState({});
+
+  useEffect(() => {
+    const fetchCustomerInfo = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/getCustomerInfo');
+        const customerData = response.data;
+        setCustomerInfo(customerData);
+      } catch (error) {
+        console.error('Error fetching customer information:', error);
+      }
+    };
+
+    fetchCustomerInfo();
+  }, []);
+
+  const { CustomerName, PortfolioValue } = customerInfo;
+
   return (
     <div className="header-container">
       <div>
-        <h1 className="d-inline-block mr-3">Customer Name</h1>
+        <h1 className="d-inline-block mr-3">{CustomerName}</h1>
+        &nbsp;
+        &nbsp;
+        {PortfolioValue && (
+          <p className="d-inline-block"> ( Portfolio Number: {PortfolioValue} )</p>
+        )}
       </div>
-      {/* <div className="d-inline-block">
-        <h5 className="mr-3">PortFolio Number</h5>
-        <h5 className="mr-3">Current Performance</h5>
-        <h5>Investment Strategy</h5>
-      </div> */}
       <button className="btn btn-primary">Add Trade</button>
     </div>
   );
